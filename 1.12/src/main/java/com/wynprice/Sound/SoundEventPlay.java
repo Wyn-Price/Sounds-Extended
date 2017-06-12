639d64e0d057fc41a78324a8e140f6929d6d2eef
Wyn Price <wynprice999@gmail.com>
2017-06-12 19:30:56 +0100

    chorted

--- 1.12/src/main/java/com/wynprice/Sound/SoundEventPlay.java
+++ 1.12/src/main/java/com/wynprice/Sound/SoundEventPlay.java
@@ -403,13 +403,8 @@ public class SoundEventPlay
 	{
 		
 		if(e.getState().getBlock() == Blocks.FIRE)
-		{
-			
 			if(!firePositions.contains(e.getPos()))
-			{
 				firePositions.add(e.getPos());
-			}
-		}
 	}
 	
 	@SubscribeEvent (priority = EventPriority.HIGHEST)
