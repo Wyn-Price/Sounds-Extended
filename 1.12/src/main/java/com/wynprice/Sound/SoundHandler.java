c4a12fc44dd2e09b0b6cd8cbfa14cb570814099d
Wyn Price <wynprice999@gmail.com>
2017-06-12 18:32:12 +0100

    Added new sounds and fixed some biomes o plenty issues

--- 1.12/src/main/java/com/wynprice/Sound/SoundHandler.java
+++ 1.12/src/main/java/com/wynprice/Sound/SoundHandler.java
@@ -20,8 +20,6 @@ public class SoundHandler
 	{
 		fireCrack = new ArrayList<SoundEvent>(Arrays.asList(register("fire.crack.1"), register("fire.crack.2"), register("fire.crack.3")));
 		highWind = new ArrayList<SoundEvent>(Arrays.asList(register("wind.high.1"), register("wind.high.2"), register("wind.high.3"), register("wind.high.4")));
-		soundForest = new ArrayList<SoundEvent>(Arrays.asList(register("forest.1"), register("forest.2"), register("forest.3"), register("forest.4"), register("forest.5"),
-				register("forest.6"), register("forest.7"), register("forest.8"), register("forest.9"), register("forest.10")));
 		soundForestStorm = new ArrayList<SoundEvent>(Arrays.asList(register("forest.storm.1")));
 		cricketNight = register("night.cricket");
 		beachWave = new ArrayList<SoundEvent>(Arrays.asList(register("beach.wave.1"), register("beach.wave.2"), register("beach.wave.3"), register("beach.wave.4"), register("beach.wave.5")));
@@ -32,6 +30,10 @@ public class SoundHandler
 		endAmbience = new ArrayList<SoundEvent>(Arrays.asList(register("end.city"), register("end.reverse")));
 		witherTimer = register("wither.spawn.timer");
 		stronghold = register("stronghold");
+		
+		soundForest = new ArrayList<SoundEvent>();
+		for(int iForest = 1; iForest <=36; iForest ++)
+			soundForest.add(register("forest." + iForest));
 	}
 	
 	public static SoundEvent register(String name)
