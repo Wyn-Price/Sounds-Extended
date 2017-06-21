d0f7a0e0b420a7189084a42dbe85d4f114feab0e
Wyn Price <wynprice999@gmail.com>
2017-06-21 20:29:16 +0100

    updated versions

--- 1.11.2/src/main/java/com/wynprice/Sound/References.java
+++ 1.11.2/src/main/java/com/wynprice/Sound/References.java
@@ -3,7 +3,7 @@ package com.wynprice.Sound;
 public class References {
 	public static final String MODID = "sounds_extended";
 	public static final String NAME = "Sounds Extended";
-	public static final String VERSION = "1.6.0-1.11.2";
+	public static final String VERSION = "1.7.0-1.11.2";
 	public static final String CLIENT_PROXY = "com.wynprice.Sound.proxys.ClientProxy";
 	public static final String SERVER_PROXY = "com.wynprice.Sound.proxys.ServerProxy";
 	public static final String GUI_FACTORY = "com.wynprice.Sound.config.guiFactory";
